aa434e036f83abe5b7a9f85eb6f721931a7d0ed8
evnwttn <evnwttn@gmail.com>
2022-07-12 20:32:11 -0230

    Update ContactForm.js

--- my-app/src/Components/ContactForm.js
+++ my-app/src/Components/ContactForm.js
@@ -3,5 +3,44 @@ import { TextField, IconButton, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 export const ContactForm = () => {
-  return <>Yo</>;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <Box
+      component="form"
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField
+        placeholder="Name"
+        variant="standard"
+        margin="normal"
+        sx={{ width: "28vw" }}
+        {...register("Name", {})}
+      />
+      <TextField
+        placeholder="Email Address"
+        variant="standard"
+        margin="normal"
+        sx={{ width: "28vw" }}
+        {...register("Email Address", {})}
+      />
+      <TextField
+        placeholder="Message"
+        variant="standard"
+        margin="normal"
+        sx={{ width: "28vw" }}
+        {...register("Message", {})}
+      />
+      <IconButton disableRipple sx={{ cursor: "default" }} type="submit">
+        <SendIcon
+          sx={{
+            ml: "25vw",
+            mt: "1vh",
+          }}
+        />
+      </IconButton>
+    </Box>
+  );
 };
